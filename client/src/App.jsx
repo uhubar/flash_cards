5cce2bb9d866fc8e044e20f9bfaac8a291fd8348
uhubar <uhubar.int@gmail.com>
2024-09-27 13:35:35 +0300

    Client home page created with routes

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Button from "./Button";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Quiz</h1>
+      <Button name="Theme 1" color="yellow" />
+      <Button name="Theme 2" color="yellow" />
+      <Button name="Theme 3" color="yellow" />
+      <div className="card">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/OneTheme.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function OneTheme({ themes }) {
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default OneTheme;
