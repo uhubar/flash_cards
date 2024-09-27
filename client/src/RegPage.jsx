f606ed10d99c28345adfc47a2a19fb9821e9be7e
uhubar <uhubar.int@gmail.com>
2024-09-27 14:20:54 +0300

    Theme page logick in progress (backup version)

---
+++ client/src/RegPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RegPage() {
+  return (
+    <div>RegPage</div>
+  )
+}
+
+export default RegPage
--- client/src/Root.jsx
+++ client/src/Root.jsx
@@ -11,7 +11,13 @@ function Root() {
               <Link to={"/"}>Main Page</Link>
             </li>
             <li>
-              <Link to={"/theme/:id"}>Theme of quiz</Link>
+              <Link to={"/theme"}>Theme of quiz</Link>
+            </li>
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+            <li>
+              <Link to={"/reg"}>Registration</Link>
             </li>
           </ul>
         </div>
